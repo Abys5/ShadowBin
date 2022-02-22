f54713268c61b31c357dcf4249a53330561e86ba
Abys5 <abyss002@pm.me>
2022-02-22 21:09:28 +0000

    Added Expiration

--- src/forms/SubmitForm.tsx
+++ src/forms/SubmitForm.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import CryptoJS from "crypto-js";
 import { firestore } from "../libs/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, FieldValue } from "firebase/firestore";
 import RichTextEditor from "@mantine/rte";
 import { Button, TextInput } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
+import dayjs from "dayjs";
 
 const SubmitForm: React.FC = () => {
     const [encrypted, setEncrypted] = useState("");
+    const [expire, setExpire] = useState(
+        dayjs(new Date()).add(1, "hour").toDate()
+    );
     const [input, setInput] = useState("");
     const [password, setPassword] = useState("");
     const [id, setId] = useState("");
@@ -20,6 +25,7 @@ const SubmitForm: React.FC = () => {
 
         const docRef = await addDoc(collection(firestore, "trash"), {
             content: encrypted,
+            expireAt: expire,
         });
 
         setId(docRef.id);
@@ -31,6 +37,16 @@ const SubmitForm: React.FC = () => {
                 label="Password / Encryption Key"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <DatePicker
+                placeholder="Pick Data"
+                label="Expiration date"
+                value={expire}
+                onChange={(date) => setExpire(date ?? expire)}
+                minDate={dayjs(new Date()).add(1, "hour").toDate()}
+                maxDate={dayjs(new Date()).add(1, "month").toDate()}
+                required
             />
             <br />
             <RichTextEditor
